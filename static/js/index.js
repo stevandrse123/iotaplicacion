5b688e90915a58e4d676781738f6caca2c8f8893
Steven Faican <safaican.fie@unach.edu.ec>
2020-09-07 14:25:05 -0500

    initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -27,8 +27,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "stevandres123@gmail.com",
+    password: "esoestodo1994",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -41,9 +41,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    client.subscribe("stevandres123@gmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "stevandres123@gmail.com/test1";
     client.send(message);
 	
   }
